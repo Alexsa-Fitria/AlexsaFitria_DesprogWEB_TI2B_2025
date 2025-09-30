36f1631ae383b829649edad4ffb7edd6e82cfd12
= <=>
2025-09-30 20:49:25 +0700

    lexsa

---
+++ jobsheet5/array_1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+
+    $listDosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    // Menampilkan array menggunakan loop foreach
+    foreach ($listDosen as $dosen) {
+        echo $dosen . "<br>";
+    }
+
+    ?>
+    
+</body>
+</html>
